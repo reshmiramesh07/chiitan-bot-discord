423a43d8c756beae734540ec3b16f73e9c9664f1
reshmiramesh07 <reshmiramesh07@gmail.com>
2024-07-18 20:32:27 -0700

    sample responses

--- responses.py
+++ responses.py
@@ -1,2 +1,14 @@
+from random import choice, randint # choice([array of strings])
+
 def get_response(user_input: str) -> str:
-    raise NotImplementedError('Code is missing...')
+    lowered: str = user_input.lower()
+
+    # REPLACE WITH RESPONSES
+    if lowered == '':
+        return ""
+    elif "hello" in lowered:
+        return "Hello there!"
+    elif "chii roll dice" in lowered:
+        return f"You rolled: {randint(1, 6)}"
+    else: 
+        return "Chiitan did not understand."
