4847112e7e0c46c2433a85bf8055b0061a5ce457
reshmiramesh07 <reshmiramesh07@gmail.com>
2024-07-18 19:36:09 -0700

    bot setup, imports

---
+++ main.py
@@ -0,0 +1,10 @@
+from typing import Final
+import os 
+from dotenv import load_dotenv
+from discord import Intents, Client, Message
+from responses import get_response
+
+# STEP 0: LOAD TOKEN FROM SOMEWHERE SAFE
+load_dotenv()
+TOKEN: Final[str] = os.getenv('DISCORD_TOKEN')
+# print(TOKEN)
