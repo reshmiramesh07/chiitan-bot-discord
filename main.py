96d56368d1ddbe013df3e444478e3b79a1fca4aa
reshmiramesh07 <reshmiramesh07@gmail.com>
2024-07-18 20:27:01 -0700

    message functionality

--- main.py
+++ main.py
@@ -13,3 +13,18 @@ TOKEN: Final[str] = os.getenv('DISCORD_TOKEN')
 intents: Intents = Intents.default()
 intents.message_content = True # NOQA
 client: Client = Client(intents=intents)
+
+# MESSAGE FUNCTIONALITY
+async def send_message(message: Message, user_message: str) -> None:
+    if not user_message:
+        print("(Message was empty because intents were not enabled probably)")
+        return
+    is_private = user_message[0] == "?"
+    if is_private:
+        user_message = user_message[1:]
+
+    try: 
+        response: str = get_response(user_message)
+        await message.author.send(response) if is_private else message.channel.send(response)
+    except Exception as e:
+        print(e)
