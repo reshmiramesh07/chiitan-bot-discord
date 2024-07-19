51cba117e419040590fedbb516d27cd472266278
reshmiramesh07 <reshmiramesh07@gmail.com>
2024-07-18 20:46:21 -0700

    bot entry, handling msgs

--- main.py
+++ main.py
@@ -25,6 +25,31 @@ async def send_message(message: Message, user_message: str) -> None:
 
     try: 
         response: str = get_response(user_message)
-        await message.author.send(response) if is_private else message.channel.send(response)
+        await message.author.send(response) if is_private else await message.channel.send(response)
     except Exception as e:
         print(e)
+
+# STARTUP FOR BOT
+@client.event
+async def on_ready() -> None:
+    print(f"{client.user} is now running!")
+
+# HANDLE INCOMING MSGS
+@client.event
+async def on_message(message: Message) -> None:
+    if message.author == client.user: # if it's the bot who sent the message, do not respond (avoid infinite loop)
+        return 
+    
+    username: str = str(message.author)
+    user_message: str = message.content
+    channel: str = str(message.channel)
+
+    print(f"[{channel}] {username}: '{user_message}'")
+    await send_message(message, user_message)
+
+# MAIN ENTRY POINT
+def main() -> None:
+    client.run(token=TOKEN)
+
+if __name__ == "__main__":
+    main()
